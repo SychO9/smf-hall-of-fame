d6faa70404dc1a44c798a4b3669904b2ee70b8fc
realdigger <digger@mysmf.net>
2018-10-05 00:05:37 +0400

    Add Russian translation.

---
+++ Themes/default/languages/Hof.russian-utf8.php
@@ -0,0 +1,46 @@
+<?php
+/* --------------------- AUTHOR:
+/* SYCHO (M.S)
+/* SMF Hall Of Fame (HOF)
+/* http://sycho.22web.org
+/* Copyright 2018
+/*
+/* Licensed under the Apache License, Version 2.0 (the "License");
+/* you may not use this file except in compliance with the License.
+/* You may obtain a copy of the License at
+/*
+/*     http://www.apache.org/licenses/LICENSE-2.0
+/*
+/* Russian translation by digger
+/* https://mysmf.net (c) 2018
+---------------------------- */
+
+$txt['hof_admin']              = "Настройки зала славы";
+$txt['hof_PageTitle']          = "Зал славы";
+$txt['hof']                    = "Зал славы";
+$txt['hof_famer']              = "Пользователь";
+$txt['hof_title']              = "Название";
+$txt['hof_description']        = "Короткое описание.";
+$txt['hof_submit']             = "Сохранить";
+$txt['hof_add_famer']          = "Добавить пользователя:";
+$txt['hof_add_class']          = "Добавить раздел:";
+$txt['hof_edit_class']         = "Редактировать раздел";
+$txt['hof_empty']              = "Показывать нечего";
+$txt['hof_empty_classes']      = "Нет разделов.";
+$txt['hof_classes']            = "Разделы";
+$txt['hof_layout']             = "Изменить вид страницы:";
+$txt['hof_delete_class']       = "Удалить раздел";
+$txt['hof_delete_famer']       = "Убрать пользователя из этого раздела";
+$txt['hof_error_unknown']      = "Что-то пошло не так!";
+$txt['hof_success']            = "Успешно выполнено";
+$txt['hof_create_class_first'] = "Сначала добавьте разделы.";
+$txt['hof_modify']             = "Редактировать";
+$txt['hof_act_deact']          = "Включить/Выключить страницу:";
+$txt['hof_activate']           = "Включить";
+$txt['hof_deactivate']         = "Выключить";
+$txt['member_since']           = "Зарегистрирован";
+$txt['hof_globalTitle']        = "Название страницы";
+$txt['hof_change_globalTitle'] = "Изменить название страницы";
+$txt['hof_ewidth']             = "Ширина колонок (Зависит от размера аватаров)";
+
+?>
